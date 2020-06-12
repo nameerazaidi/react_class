e84e985a40a04b8e5913d04d4029a84704e1c4f3
namee <nameerazaidi@gmail.com>
2020-06-12 18:56:59 +0500

    update app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      Hello From React App from Nameera
+      Hello From React App from Nameera I am goung to create Covid 19 App
     </div>
   );
 }
