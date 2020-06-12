98bde44c963a4eb510b3a38d2b198884a03b384e
namee <nameerazaidi@gmail.com>
2020-06-12 20:21:45 +0500

    new dinner and update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import './App.css';
+import Dinner from './dinner.js';
 
 function App() {
   return (
     <div className="App">
       Hello From React App from Nameera I am goung to create Covid 19 App
+      <Dinner dishname="Salad and" sweet="Kheer"/>
+      <hr/>
+      <Dinner dishname="Cold drink and" sweet="custured"/>
+      <hr/>
+      <Dinner dishname="Raita and" sweet="fruitchart"/>
+
     </div>
   );
 }
