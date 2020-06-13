d9744cbe7231b9f656f601cba75482c73209090a
namee <nameerazaidi@gmail.com>
2020-06-13 22:42:46 +0500

    class 3 and 4 assignments

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import Teacher from './teacher.js';
 
 function App() {
+  let [count,setCount]=useState(0);
+  let [isYellow,setYellow]=useState(false);
   return (
-    <div className="App">
-      Hello From React App from Nameera I am goung to create Covid 19 App
+    <div className={`box ${isYellow ? 'yellow' : 'pink' }`} >
+      <h2>Hello  React App from Nameera <br />Class 4</h2>
       <Teacher Tchname="Nameerah" subject="Computer" Zoomid="12345678" Zoompsd="asdfgh" />
       <hr/>
-      <Teacher Tchname="Sehrish" subject="Science"  Zoomid="09876543" Zoompsd="qwerty" />
+      <Teacher Tchname="Sana" subject="Science"  Zoomid="09876543" Zoompsd="qwerty" />
       <hr/>
-      <Teacher Tchname="Naila" subject="Maths" Zoomid="45678990" Zoompsd="zxcvbn" />
-
+      <Teacher Tchname="Saba" subject="Maths" Zoomid="45678990" Zoompsd="zxcvbn" />
+      
+      <h3> Counter Value = {count}</h3>
+      <p><button onClick={()=>setCount(count+1)} className={"btn"}>Add</button>      
+      &nbsp;&nbsp;&nbsp;
+      <button onClick={()=>setCount(count-1)} className={"btn"}>Subtract</button></p>
+   
+      <h2>Box Colour = {isYellow ? 'Yellow Colour':'Pink Colour'}</h2>
+      <button onClick={()=>setYellow(true)} className={"yellow"}>  Yellow  </button>
+      &nbsp;&nbsp;
+      <button onClick={()=>setYellow(false)} className={"pink"}> Pink  </button>
     </div>
   );
 }
--- src/teacher.js
+++ src/teacher.js
@@ -3,10 +3,10 @@ function Teacher(props)
 {
     return(
         <div align="left">
-        <h2>Teacher Name:  {props.Tchname}</h2>
-        <h3>Subject: {props.subject}</h3>
-        <h3>Zoom Meeting id: {props.Zoomid}</h3>
-        <h3>Zoom Password: {props.Zoompsd}</h3>
+        <b>Teacher Name:  {props.Tchname}</b>
+        <br />Subject: {props.subject}
+        <br />Zoom Meeting id: {props.Zoomid}
+        <br />Zoom Password: {props.Zoompsd}
         </div>
     );
 }
